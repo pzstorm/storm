d7bb9bf202caa45fbd24a374fb144d359e51d48c
Matthew <yooks@tuta.io>
2021-05-02 21:17:13 +0200

    Create ASM utility class

---
+++ src/main/java/io/pzstorm/storm/util/AsmUtils.java
@@ -0,0 +1,14 @@
+package io.pzstorm.storm.util;
+
+import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.tree.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class contains an assortment of helpful methods when working with ASM.
+ */
+public class AsmUtils {
+
+}
