48256824fbadd4acd8d68cc9efac980e91cd09a8
Matthew <yooks@tuta.io>
2021-05-07 19:30:36 +0200

    Suppress unused methods warnings

--- src/test/java/io/pzstorm/storm/util/StormUtilsTest.java
+++ src/test/java/io/pzstorm/storm/util/StormUtilsTest.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("unused")
 class StormUtilsTest {
 
 	private boolean hasInvokedRestrictedMethod = false;
