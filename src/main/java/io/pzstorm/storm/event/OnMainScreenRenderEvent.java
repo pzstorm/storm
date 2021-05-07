91fde4fedd26e7b9b4648fabeaee74b33344352d
Matthew <yooks@tuta.io>
2021-05-07 18:08:53 +0200

    Add first zomboid render events

---
+++ src/main/java/io/pzstorm/storm/event/OnMainScreenRenderEvent.java
@@ -0,0 +1,21 @@
+package io.pzstorm.storm.event;
+
+import zombie.gameStates.MainScreenState;
+
+/**
+ * This event fires on each tick when the main screen is being rendered.
+ *
+ * @see MainScreenState#render()
+ */
+public class OnMainScreenRenderEvent implements ZomboidEvent {
+
+	@Override
+	public String getName() {
+		return "onMainScreenRender";
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
---
+++ src/main/java/io/pzstorm/storm/event/OnUIElementPreRenderEvent.java
@@ -0,0 +1,28 @@
+package io.pzstorm.storm.event;
+
+import zombie.ui.UIElement;
+
+/**
+ * This event fires when an {@link UIElement} is being pre-rendered.
+ *
+ * @see UIElement#render()
+ */
+@SuppressWarnings("WeakerAccess")
+public class OnUIElementPreRenderEvent implements ZomboidEvent {
+
+	public final UIElement element;
+
+	public OnUIElementPreRenderEvent(UIElement element) {
+		this.element = element;
+	}
+
+	@Override
+	public String getName() {
+		return "onUIElementPreRender";
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
