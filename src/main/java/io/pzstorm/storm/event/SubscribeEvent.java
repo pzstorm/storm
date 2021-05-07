2ccd9fd4e6c55f1b704c698f218ce5ca05c07444
Matthew <yooks@tuta.io>
2021-05-07 18:02:17 +0200

    Create SubscribeEvent interface class

---
+++ src/main/java/io/pzstorm/storm/event/SubscribeEvent.java
@@ -0,0 +1,18 @@
+package io.pzstorm.storm.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to subscribe methods to {@link ZomboidEvent} types. Methods are registered
+ * in bulk via event handler classes. To register an event handler call an appropriate class in
+ * {@link StormEventDispatcher}. Once the event handler has been registered all methods belonging to
+ * the event handler annotated with {@link SubscribeEvent} will be subscribed to events they specify
+ * as method parameters. Read more about this process in {@link StormEventDispatcher} class documentation.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SubscribeEvent {
+}
