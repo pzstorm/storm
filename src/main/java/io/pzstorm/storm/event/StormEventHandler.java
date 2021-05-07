1a6bba2b7d6f296b728736bb20426b654ec162a3
Matthew <yooks@tuta.io>
2021-05-07 19:22:19 +0200

    Create StormEventHandler class

---
+++ src/main/java/io/pzstorm/storm/event/StormEventHandler.java
@@ -0,0 +1,11 @@
+package io.pzstorm.storm.event;
+
+/**
+ * This class responds to all events needed for Storm to implement custom features.
+ * Note that not all functionality implementations that are weaved into game bytecode is handled here.
+ * Sometimes subscribing to events is not enough to alter game behavior and more invasive
+ * actions need to be preformed, like editing or removing lines from game code.
+ */
+public class StormEventHandler {
+
+}
