d139b3e8e553daeeafaaba81750c29c1ed2e9222
Matthew <yooks@tuta.io>
2021-05-07 18:15:19 +0200

    Add first render event hooks

---
+++ src/main/java/io/pzstorm/storm/hook/OnMainScreenRenderHook.java
@@ -0,0 +1,41 @@
+package io.pzstorm.storm.hook;
+
+import com.google.common.collect.ImmutableList;
+import io.pzstorm.storm.core.StormClassTransformer;
+import io.pzstorm.storm.event.OnMainScreenRenderEvent;
+import io.pzstorm.storm.event.StormEventDispatcher;
+import io.pzstorm.storm.util.AsmUtils;
+import io.pzstorm.storm.util.StormUtils;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+public class OnMainScreenRenderHook implements StormHook {
+
+	@Override
+	public void installHook(StormClassTransformer transformer) {
+
+		// public void render()
+		InsnList instructions = transformer.getInstructionsForMethod("render", "()V");
+		String eventDescriptor = StormUtils.getClassAsPath(OnMainScreenRenderEvent.class);
+
+		// new OnMainScreenRenderEvent()
+		InsnList dispatchOnRenderEvent = StormEventDispatcher.callDispatchEvent(ImmutableList.of(
+				new TypeInsnNode(Opcodes.NEW, eventDescriptor), new InsnNode(Opcodes.DUP),
+				new MethodInsnNode(Opcodes.INVOKESPECIAL, eventDescriptor, "<init>", "()V")
+		));
+		// ...
+		// -> insert <-
+		// Core.getInstance().EndFrameUI();
+		// ...
+		LabelNode target = AsmUtils.getFirstMatchingLabelNode(instructions, ImmutableList.of(
+				// INVOKESTATIC zombie/core/Core.getInstance ()Lzombie/core/Core;
+				new MethodInsnNode(Opcodes.INVOKESTATIC,
+						"zombie/core/Core", "getInstance", "()Lzombie/core/Core;"
+				),
+				// INVOKEVIRTUAL zombie/core/Core.EndFrameUI ()V
+				new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
+						"zombie/core/Core", "EndFrameUI", "()V"))
+		);
+		instructions.insertBefore(target, dispatchOnRenderEvent);
+	}
+}
---
+++ src/main/java/io/pzstorm/storm/hook/OnUIElementPreRenderHook.java
@@ -0,0 +1,56 @@
+package io.pzstorm.storm.hook;
+
+import com.google.common.collect.ImmutableList;
+import io.pzstorm.storm.core.StormClassTransformer;
+import io.pzstorm.storm.event.OnUIElementPreRenderEvent;
+import io.pzstorm.storm.event.StormEventDispatcher;
+import io.pzstorm.storm.util.AsmUtils;
+import io.pzstorm.storm.util.StormUtils;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+public class OnUIElementPreRenderHook implements StormHook {
+
+	@Override
+	public void installHook(StormClassTransformer transformer) {
+
+		// public void render()
+		InsnList instructions = transformer.getInstructionsForMethod("render", "()V");
+		String eventDescriptor = StormUtils.getClassAsPath(OnUIElementPreRenderEvent.class);
+
+		// new OnUIElementPreRenderEvent(UIElement)
+		InsnList dispatchOnUIElementPreRenderEvent = StormEventDispatcher.callDispatchEvent(ImmutableList.of(
+				new TypeInsnNode(Opcodes.NEW, eventDescriptor),
+				new InsnNode(Opcodes.DUP), new VarInsnNode(Opcodes.ALOAD, 0),
+				new MethodInsnNode(Opcodes.INVOKESPECIAL,
+						eventDescriptor, "<init>", "(Lzombie/ui/UIElement;)V")
+		));
+		// ...
+		// LuaManager.caller.pcallvoid(UIManager.getDefaultThread(), this.getTable().rawget("prerender"), this.table);
+		// -> insert <-
+		// ...
+		LabelNode callPrerender = AsmUtils.getFirstMatchingLabelNode(instructions, ImmutableList.of(
+				new FieldInsnNode(Opcodes.GETSTATIC, "zombie/Lua/LuaManager", "caller",
+						"Lse/krka/kahlua/integration/LuaCaller;"
+				),
+				new MethodInsnNode(Opcodes.INVOKESTATIC, "zombie/ui/UIManager", "getDefaultThread",
+						"()Lse/krka/kahlua/vm/KahluaThread;"
+				),
+				new VarInsnNode(Opcodes.ALOAD, 0),
+				new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "zombie/ui/UIElement", "getTable",
+						"()Lse/krka/kahlua/vm/KahluaTable;"
+				),
+				new LdcInsnNode("prerender"),
+				new MethodInsnNode(Opcodes.INVOKEINTERFACE, "se/krka/kahlua/vm/KahluaTable", "rawget",
+								"(Ljava/lang/Object;)Ljava/lang/Object;", true
+				),
+				new VarInsnNode(Opcodes.ALOAD, 0),
+				new FieldInsnNode(Opcodes.GETFIELD, "zombie/ui/UIElement", "table",
+						"Lse/krka/kahlua/vm/KahluaTable;"
+				),
+				new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "se/krka/kahlua/integration/LuaCaller", "pcallvoid",
+						"(Lse/krka/kahlua/vm/KahluaThread;Ljava/lang/Object;Ljava/lang/Object;)V"))
+		);
+		instructions.insert(callPrerender, dispatchOnUIElementPreRenderEvent);
+	}
+}
