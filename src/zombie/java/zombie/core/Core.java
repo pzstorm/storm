cbcf4cc673f64fd0664b8f5c04e9d5e9da42c8f7
Matthew <yooks@tuta.io>
2021-05-10 22:40:37 +0200

    Add zomboid dummy classes

--- src/zombie/java/zombie/core/Core.java
+++ src/zombie/java/zombie/core/Core.java
@@ -6,6 +6,8 @@ import zombie.ZomboidClass;
 @SuppressWarnings("ALL")
 public class Core {
 
+	public static boolean bDebug = false;
+
 	public static int width = 0;
 	public static int height = 0;
 }
