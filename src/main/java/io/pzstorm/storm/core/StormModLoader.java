4d6d10fa71003c4beeb379aff6deba4e827972a4
Matthew <yooks@tuta.io>
2021-05-13 07:22:19 +0200

    Create StormModLoader core class

---
+++ src/main/java/io/pzstorm/storm/core/StormModLoader.java
@@ -0,0 +1,11 @@
+/**
+ * This class is responsible for registering and loading mod components:
+ * <ul>
+ * <li>Catalog mod jars by mapping them to mod directory name.</li>
+ * <li>Catalog mod metadata by mapping them to mod directory name.</li>
+ * <li>Catalog mod classes by mapping them to mod directory name.</li>
+ * <li>Load mod classes with {@link StormClassLoader}.</li>
+ * </ul>
+ */
+class StormModLoader {
+}
