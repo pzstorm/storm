fc66c550475f94c9370e1f9abdfe49f45d24cb96
Matthew <yooks@tuta.io>
2021-05-15 21:34:14 +0200

    Create mod entry point class

---
+++ src/api/java/io/pzstorm/storm/mod/ZomboidMod.java
@@ -0,0 +1,11 @@
+package io.pzstorm.storm.mod;
+
+/**
+ * This class represents a Project Zomboid Java mod entry point. Every mod is expected to have
+ * a single class that implements this class. Mods that do not implement this class will not be
+ * registered and will not be eligible to subscribe to events.
+ */
+public interface ZomboidMod {
+
+	void registerEventHandlers();
+}
