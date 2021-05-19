07038b15efbaa93e5b96f395d2d3b97c9d78ad8d
Matthew <yooks@tuta.io>
2021-05-20 01:04:02 +0200

    Write note about creating LuaEvents

--- api/src/main/java/io/pzstorm/storm/event/lua/LuaEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/LuaEvent.java
@@ -4,11 +4,15 @@ import io.pzstorm.storm.event.ZomboidEvent;
 import zombie.Lua.LuaEventManager;
 
 /**
- * This class represents an in-game event dispatched to Lua mods.
- * Lua events are handled by {@link LuaEventManager}. Storm intercepts these
- * events and dispatches them as {@link LuaEvent} implementation instances.
- * Subscribing to events being defined as Java classes offers much easier
- * and safer interaction to subscribers then when subscribing from Lua.
+ * <p>This class represents an in-game event dispatched to Lua mods. Lua events are handled by
+ * {@link LuaEventManager}. Storm intercepts these events and dispatches them as {@link LuaEvent}
+ * implementation instances. Subscribing to events being defined as Java classes offers much
+ * easier and safer interaction to subscribers then when subscribing from Lua.</p>
+ * <h3>Note for developers</h3>
+ * <p>When writing new event implementation classes do not forget to add the class
+ * references to {@link LuaEventFactory} static block so that the references are
+ * properly mapped and recognized by factory. If the references are not added the
+ * factory will not be able create new instances of those event.</p>
  */
 public interface LuaEvent extends ZomboidEvent {
 
