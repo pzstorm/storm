859d0553bf13586e08606853b751013bca31dbbe
Matthew <yooks@tuta.io>
2021-05-21 00:24:49 +0200

    Add more event documentation

--- api/src/main/java/io/pzstorm/storm/event/lua/OnKeyPressedEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnKeyPressedEvent.java
@@ -8,6 +8,10 @@ package io.pzstorm.storm.event.lua;
 @SuppressWarnings({ "WeakerAccess", "unused" })
 public class OnKeyPressedEvent implements LuaEvent {
 
+	/**
+	 * <p>Integer denoting the key being pressed.</p>
+	 * See {@code org.lwjgl.input.Keyboard} for list of key codes.
+	 */
 	public final int key;
 
 	public OnKeyPressedEvent(int key) {
--- api/src/main/java/io/pzstorm/storm/event/lua/OnKeyStartPressedEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnKeyStartPressedEvent.java
@@ -8,6 +8,10 @@ package io.pzstorm.storm.event.lua;
 @SuppressWarnings({ "WeakerAccess", "unused" })
 public class OnKeyStartPressedEvent implements LuaEvent {
 
+	/**
+	 * <p>Integer denoting the key being pressed.</p>
+	 * See {@code org.lwjgl.input.Keyboard} for list of key codes.
+	 */
 	public final int key;
 
 	public OnKeyStartPressedEvent(int key) {
