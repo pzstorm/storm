202555c83415c4b6e553683681e3aed3ad193a35
Matthew <yooks@tuta.io>
2021-05-21 03:22:46 +0200

    Make LuaEvent fields more inclusive

--- api/src/main/java/io/pzstorm/storm/event/lua/OnContainerUpdateEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnContainerUpdateEvent.java
@@ -3,7 +3,7 @@ package io.pzstorm.storm.event.lua;
 import org.jetbrains.annotations.Nullable;
 import zombie.inventory.InventoryItem;
 import zombie.iso.IsoGridSquare;
-import zombie.iso.objects.IsoDeadBody;
+import zombie.iso.IsoObject;
 
 /**
  * Triggered when an inventory container is being updated.
@@ -22,19 +22,19 @@ public class OnContainerUpdateEvent implements LuaEvent {
 	public final @Nullable InventoryItem item;
 
 	/**
-	 * Dead body that triggered the container update.
+	 * Object that triggered the container update.
 	 */
-	public final @Nullable IsoDeadBody deadBody;
+	public final @Nullable IsoObject object;
 
 	private OnContainerUpdateEvent(@Nullable IsoGridSquare gridSquare,
-								  @Nullable InventoryItem item, @Nullable IsoDeadBody deadBody) {
+								  @Nullable InventoryItem item, @Nullable IsoObject object) {
 
 		this.gridSquare = gridSquare;
 		this.item = item;
-		this.deadBody = deadBody;
+		this.object = object;
 	}
 
-	public OnContainerUpdateEvent(@Nullable IsoDeadBody deadBody) {
+	public OnContainerUpdateEvent(@Nullable IsoObject deadBody) {
 		this(null, null, deadBody);
 	}
 
