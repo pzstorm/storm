ef3a4ab585fe07c3a9b17224d37d933b8a6e4bcf
Matthew <yooks@tuta.io>
2021-05-21 03:15:57 +0200

    Define missing LuaEvent fields

--- api/src/main/java/io/pzstorm/storm/event/lua/OnContainerUpdateEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnContainerUpdateEvent.java
@@ -1,7 +1,9 @@
 package io.pzstorm.storm.event.lua;
 
 import org.jetbrains.annotations.Nullable;
+import zombie.inventory.InventoryItem;
 import zombie.iso.IsoGridSquare;
+import zombie.iso.objects.IsoDeadBody;
 
 /**
  * Triggered when an inventory container is being updated.
@@ -14,11 +16,37 @@ public class OnContainerUpdateEvent implements LuaEvent {
 	 */
 	public final @Nullable IsoGridSquare gridSquare;
 
-	public OnContainerUpdateEvent(@Nullable IsoGridSquare gridSquare) {
+	/**
+	 * Item that triggered the container update.
+	 */
+	public final @Nullable InventoryItem item;
+
+	/**
+	 * Dead body that triggered the container update.
+	 */
+	public final @Nullable IsoDeadBody deadBody;
+
+	private OnContainerUpdateEvent(@Nullable IsoGridSquare gridSquare,
+								  @Nullable InventoryItem item, @Nullable IsoDeadBody deadBody) {
+
 		this.gridSquare = gridSquare;
+		this.item = item;
+		this.deadBody = deadBody;
+	}
+
+	public OnContainerUpdateEvent(@Nullable IsoDeadBody deadBody) {
+		this(null, null, deadBody);
+	}
+
+	public OnContainerUpdateEvent(@Nullable InventoryItem item) {
+		this(null, item, null);
+	}
+
+	public OnContainerUpdateEvent(@Nullable IsoGridSquare gridSquare) {
+		this(gridSquare, null, null);
 	}
 
 	public OnContainerUpdateEvent() {
-		this(null);
+		this(null, null, null);
 	}
 }
