d1e3e33c6b68e0f80c355d92c1ff5a2e8acc206b
Matthew <yooks@tuta.io>
2021-05-30 14:21:36 +0200

    Add missing event constructors
    Resolves: #12

--- api/src/main/java/io/pzstorm/storm/event/lua/OnFillWorldObjectContextMenuEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnFillWorldObjectContextMenuEvent.java
@@ -18,6 +18,28 @@
 
 package io.pzstorm.storm.event.lua;
 
+import se.krka.kahlua.vm.KahluaTable;
+
+/**
+ * <p>Triggered when a player right clicks a world item.</p>
+ * Use the event to add items to context menu without mod conflicts.
+ */
+@SuppressWarnings({ "WeakerAccess", "unused" })
 public class OnFillWorldObjectContextMenuEvent implements LuaEvent {
-	// TODO: finish writing this event
+
+	/**
+	 * Index of player that triggered the event.
+	 */
+	public final Double playerIndex;
+
+	// TODO: finish documenting this event
+	public final KahluaTable context, worldObjects;
+	public final Boolean isTest;
+
+	public OnFillWorldObjectContextMenuEvent(Double arg1, KahluaTable context, KahluaTable worldObjects, Boolean isTest) {
+		this.playerIndex = arg1;
+		this.context = context;
+		this.worldObjects = worldObjects;
+		this.isTest = isTest;
+	}
 }
--- api/src/main/java/io/pzstorm/storm/event/lua/OnPreFillWorldObjectContextMenuEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/lua/OnPreFillWorldObjectContextMenuEvent.java
@@ -18,6 +18,25 @@
 
 package io.pzstorm.storm.event.lua;
 
+import se.krka.kahlua.vm.KahluaTable;
+
+// TODO: finish documenting this event
+@SuppressWarnings({ "WeakerAccess", "unused" })
 public class OnPreFillWorldObjectContextMenuEvent implements LuaEvent {
-	// TODO: finish writing this event
+
+	/**
+	 * Index of player that triggered the event.
+	 */
+	public final Double playerIndex;
+
+	// TODO: finish documenting this event
+	public final KahluaTable context, worldObjects;
+	public final Boolean isTest;
+
+	public OnPreFillWorldObjectContextMenuEvent(Double arg1, KahluaTable context, KahluaTable worldObjects, Boolean isTest) {
+		this.playerIndex = arg1;
+		this.context = context;
+		this.worldObjects = worldObjects;
+		this.isTest = isTest;
+	}
 }
