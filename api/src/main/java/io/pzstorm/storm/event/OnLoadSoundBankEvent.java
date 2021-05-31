0650eefdca1d8bdfa359ee61b33439681c9eaab0
Matthew <yooks@tuta.io>
2021-05-31 23:22:35 +0200

    Rename OnLoadSoundBanks event

--- api/src/main/java/io/pzstorm/storm/event/OnLoadSoundBankEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/OnLoadSoundBankEvent.java
@@ -24,7 +24,7 @@ import io.pzstorm.storm.event.lua.LuaEvent;
  * Triggered after sound banks have been loaded.
  */
 @SuppressWarnings({ "WeakerAccess", "unused" })
-public class OnLoadSoundBanksEvent implements LuaEvent {
+public class OnLoadSoundBankEvent implements LuaEvent {
 
 	// TODO: this event is not actually triggered directly after sound banks have been loaded,
 	//  	so move this event to be triggered directly after sound banks have been loaded
