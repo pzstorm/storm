1c8ce7e97819db5f04760022b4efa41ab090e8c0
Matthew <yooks@tuta.io>
2021-05-31 23:04:07 +0200

    Replace OnLoadSoundBanks event trigger
    This event trigger was not placed correctly.

---
+++ core/src/main/java/io/pzstorm/storm/patch/GameWindowPatch.java
@@ -0,0 +1,44 @@
+/*
+ * Zomboid Storm - Java modding toolchain for Project Zomboid
+ * Copyright (C) 2021 Matthew Cain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.pzstorm.storm.patch;
+
+import io.pzstorm.storm.core.StormClassTransformer;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.LdcInsnNode;
+
+public class GameWindowPatch implements ZomboidPatch {
+
+	@Override
+	public void applyPatch(StormClassTransformer transformer) {
+
+		InsnList initMethodInsn = transformer.getInstructionsForMethod("init", "()V");
+		for (AbstractInsnNode instruction : initMethodInsn)
+		{
+			// replace OnLoadSoundBanks with OnGameWindowInit event
+			if (instruction instanceof LdcInsnNode)
+			{
+				LdcInsnNode ldcNode = ((LdcInsnNode)instruction);
+				if (ldcNode.cst.equals("OnLoadSoundBanks")) {
+					ldcNode.cst = "OnGameWindowInit";
+				}
+			}
+		}
+	}
+}
