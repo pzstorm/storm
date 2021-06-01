e536044727b7f1f1fdba4528c16aed063480d788
Matthew <yooks@tuta.io>
2021-06-01 19:40:54 +0200

    Create OnLoadSoundBank event
    Resolves: #14

--- api/src/main/java/io/pzstorm/storm/event/OnLoadSoundBankEvent.java
+++ api/src/main/java/io/pzstorm/storm/event/OnLoadSoundBankEvent.java
@@ -21,11 +21,24 @@ package io.pzstorm.storm.event;
 import io.pzstorm.storm.event.lua.LuaEvent;
 
 /**
- * Triggered after sound banks have been loaded.
+ * This event triggers when a sound bank is being loaded.
+ * Users can use this event to change the path of sound banks being loaded,
+ * effectively replacing vanilla with their own custom sound banks.
  */
 @SuppressWarnings({ "WeakerAccess", "unused" })
 public class OnLoadSoundBankEvent implements LuaEvent {
 
-	// TODO: this event is not actually triggered directly after sound banks have been loaded,
-	//  	so move this event to be triggered directly after sound banks have been loaded
+	/**
+	 * {@code StringBuffer} containing the string that denotes the path to the sound bank being loaded.
+	 * In order to replace the given path with a custom path you can do the following:
+	 * <pre>
+	 * String customPath = "path/to/custom/sound/bank";
+	 * soundBankPath.delete(0, soundBankPath.length()).append(customPath);
+	 * </pre>
+	 */
+	public final StringBuffer soundBankPath;
+
+	public OnLoadSoundBankEvent(StringBuffer soundBankPath) {
+		this.soundBankPath = soundBankPath;
+	}
 }
